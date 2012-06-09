eca4c48a86d0c7b7a32531accea31e70111aaead
nqpromo <nqpromo@promo2.(none)>
2012-06-10 08:49:25 +1000

    add request login page. create page_dialog template for login style dialog presentation. adjust login view to fit in with this.

---
+++ application/dev/views/system/page_dialog.php
@@ -0,0 +1,20 @@
+<!doctype html>
+<html>
+  <head>
+    <title><?= $page_name ?> :: DDD-Store</title>
+    <link href="/styles/<?= $theme ?>/core.css" type="text/css" rel="stylesheet" />
+  </head>
+  <body>
+    <div class="container">
+	  <div class="row">
+
+		<div class="span7 offset2 top1 well-well">
+
+		  <?= $content ?>
+
+		</div>
+
+	  </div>
+    </div>
+  </body>
+</html>
--- application/dev/views/system/request_login.php
+++ application/dev/views/system/request_login.php
@@ -1,7 +1,34 @@
-<?php
+<div class="DWarrior">&nbsp;</div>
+<h2 class="org"><?= $org_name ?></h2>
 
-/*
- * To change this template, choose Tools | Templates
- * and open the template in the editor.
- */
-?>
+<div class="span4">
+  <p>To get a login to this store you will need to contact the program coordinator.</p>
+  <table class="table table-striped table-bordered">
+	<tr>
+	  <th>Contact</th>
+	  <td>
+		<strong>Nelson Lee</strong><br>
+		Tackling Smoking and<br>Healthy Lifestyle Program<br>
+		Wuchopperen Health Service		
+	  </td>
+	</tr>
+	<tr>
+	  <th>Phone</th>
+	  <td>07 4080 1036</td>
+	</tr>
+	<tr>
+	  <th>Email</th>
+	  <td>
+		<script type="text/javascript">
+		var e = 'n' + 'lee' + '&#64;' + 'wuchopperen&#46;com';
+		document.write('<a href="mailto:' + e + '">');
+		document.write(e);
+		document.write('</a>');
+		</script>
+	  </td>
+	</tr>
+  </table>
+  <p>
+	
+  </p>
+</div>
