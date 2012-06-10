543dee41f43548548269e98c146f677d73414677
nqpromo <nqpromo@promo2.(none)>
2012-06-10 20:20:24 +1000

    tidy up old ref to static User class.

--- application/dev/controllers/store.php
+++ application/dev/controllers/store.php
@@ -20,7 +20,7 @@ class Store extends MM_controller {
   }
   
   function renderHTMLEntity() {
-	
+
 	$this->load->model('m_supplier_freights');
 	$this->load->model('m_metas');
 	
@@ -35,7 +35,8 @@ class Store extends MM_controller {
 	$this->load->vars('imageJSON', $this->m_product_metas->imageJSON());
 
 	$this->m_supplier_freights->suppliersID = $product->suppliersID;
-	$this->m_supplier_freights->zonesID = User::zoneID();
+//print 'renderHTMLEntity'; exit;
+	$this->m_supplier_freights->zonesID = $this->user->zoneID();
 	$this->load->vars('freightJSON', json_encode($this->m_supplier_freights->getJoined()));
 
 	$this->load->vars('js_tpl_entity', $this->load->view('store/js_tpl_entity','',true));
