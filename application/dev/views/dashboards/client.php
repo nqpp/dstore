41d1374528ac4d4bf4cfec19ab563f27c7c6bdb8
nqpromo <nqpromo@promo2.(none)>
2012-06-10 14:45:14 +1000

    Add static content to client dashboard page.

--- application/dev/views/dashboards/client.php
+++ application/dev/views/dashboards/client.php
@@ -1,1 +1,25 @@
-<h1>Client Dashboard</h1>
+<div class="row row-padded">&nbsp;</div>
+<div class="row">
+
+  <div class="span8">
+	<p><img src="/images/DWarriorLogo/0/380/1/DWarriorLogo.png"></p>
+
+	<p class="lead">As part of <em>Closing the Gap</em> initiative in Tackling Indigenous Chronic Disease, Far North Regional Tackling Smoking and Healthy Lifestyle Team has created the D Warrior Character utilising it as a means to deliver its messages to the Aboriginal and Torres Strait Islander communities in the region and more broadly, making an impact to wider communities right across the nation.</p>
+	<p>In bringing this character to life, the purpose is for the D Warrior to act as a message stick that delivers positive messages to community's and it's people to stay healthy, deadly and strong.</p>
+	<p>The aims for the creation of such a superhero character and materialising this character in all communities is to act as the educator at the forefront of harm associated with smoking tobacco and to relay key messages that highlight the 3 D's of:</p>
+	
+	<ul class="lead">
+	  <li>Durries Do Destroy,</li>
+	  <li>Danger from Tobacco Smoking,</li>
+	  <li>Diseases from Tobacco Smoking</li>
+	</ul>
+	
+	<p>We hope that the Superhero character can also deliver its messages <strong>(Tackling Tobacco Smoking Head On)</strong> to the wider community as tobacco smoking are not just an Aboriginal and Torres Strait Islander people's problem but is something that affects everyone.</p>
+  </div>
+
+  <div class="span4">
+	<img src="/images/DWarriorCharacter/2/185/400/DWarriorCharacter.png">
+
+  </div>
+
+</div>
